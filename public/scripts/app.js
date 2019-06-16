52869016c1acd17683934731f628c57a518535f2
jelocodes <michaelangelo.yambao@gmail.com>
2019-06-16 19:43:33 +0000

    can asynchronously render new tweets from database with renderTweets function

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -19,26 +19,81 @@ $(document).ready(function(){
     return $tweetArticle.append($header).append($tweet).append($footer);
   }
 
-  const tweetData = {
-    "user": {
-      "name": "Newton",
-      "avatars": {
-        "small":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
-        "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
-        "large":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
+  // const tweetData = {
+  //   "user": {
+  //     "name": "Newton",
+  //     "avatars": {
+  //       "small":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
+  //       "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
+  //       "large":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
+  //     },
+  //     "handle": "@SirIsaac"
+  //   },
+  //   "content": {
+  //     "text": "If I have seen further it is by standing on the shoulders of giants"
+  //   },
+  //   "created_at": 1461116232227
+  // }
+
+  const data = [
+    {
+      "user": {
+        "name": "Newton",
+        "avatars": {
+          "small":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
+          "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
+          "large":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
+        },
+        "handle": "@SirIsaac"
       },
-      "handle": "@SirIsaac"
+      "content": {
+        "text": "If I have seen further it is by standing on the shoulders of giants"
+      },
+      "created_at": 1461116232227
     },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
+    {
+      "user": {
+        "name": "Descartes",
+        "avatars": {
+          "small":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_50.png",
+          "regular": "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc.png",
+          "large":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_200.png"
+        },
+        "handle": "@rd" },
+      "content": {
+        "text": "Je pense , donc je suis"
+      },
+      "created_at": 1461113959088
     },
-    "created_at": 1461116232227
+    {
+      "user": {
+        "name": "Johann von Goethe",
+        "avatars": {
+          "small":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_50.png",
+          "regular": "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1.png",
+          "large":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_200.png"
+        },
+        "handle": "@johann49"
+      },
+      "content": {
+        "text": "Es ist nichts schrecklicher als eine tätige Unwissenheit."
+      },
+      "created_at": 1461113796368
+    }
+  ];
+
+  const renderTweets = (tweetObjectArray) => {
+    for (tweetObject of tweetObjectArray) {
+      $('#tweet-container').append(createTweetElement(tweetObject));
+    }
   }
 
-  const $tweet = createTweetElement(tweetData);
+  // const $tweet = createTweetElement(tweetData);
 
-  console.log($tweet);
+  // console.log($tweet);
 
-  $('#tweet-container').append($tweet);
+  // $('#tweet-container').append($tweet);
+
+  renderTweets(data);
 })
 
