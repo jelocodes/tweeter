986abc581fc6133cd1872954ecb91b8339c752b5
jelocodes <michaelangelo.yambao@gmail.com>
2019-06-17 01:17:46 +0000

    add some commenting for character counter function

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,7 +1,13 @@
 $(document).ready(function() {
+
+  // Select the character counter DOM element
   const domElem = $('span.counter');
+
+  // When the input box changes, subtract the current length of its contents from 140 and update the character counter
   $('textarea').on('input propertychange', () => {
     domElem[0].innerText = 140 - $('textarea').val().length;
+
+    // Change the character counter to red when the user has run out of characters
     (parseInt(domElem[0].innerText) < 0) ? domElem.css('color', 'red') : domElem.css('color', 'black');
     $('#error').text('');
   });
